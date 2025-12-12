4016d3ee0bfb94c427994e3249763d999f421c88
tjkim96 <tjkim.main@gmail.com>
2025-12-11 23:34:47 -0500

    first commit

--- scripts/map.js
+++ scripts/map.js
@@ -7,22 +7,61 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let zoomLevel = 17;
 
-    /* create Leaflet map object, 
-        place in container, 
-        and set initial center and zoom */
+    /* create Leaflet map object */
     let map = L.map('map_container').setView(centerView, zoomLevel);
 
     /* required attribution */
-    let attributionHtml = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    let osmAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
-    /* create tile layer with attribution */
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: attributionHtml }).addTo(map);
+    /* --- Base Layers --- */
 
-    /* create marker, info popup, and add to map */
-    L.marker(smithPlayground).bindPopup("Smith Playground - <a href='https://www.boston.gov/news/smith-playground-allston-reopened'>Renovated in 2019,</a> there are amenities for many sports, including 2 basketball courts, 2 softball fields, a skateboarding track, a street hockey and roller skating/rollerblading rink, an athletic track, and plenty of open field space for soccer and other activities.").addTo(map);
+    // 1) Standard OpenStreetMap layer
+    let osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: osmAttribution
+    });
 
-    L.marker(harvardStadium).bindPopup("Harvard Stadium - Where the Harvard football team plays. For a grueling workout, try the <a href='https://medium.com/@michaelwoodfitness/one-of-the-best-workouts-you-can-do-stadium-stair-running-or-walking-3d86c4a245c3'>'tour de stade'</a>, where participants ascend and descend the stairs in each seating section (for a total of ~1,150 steps).").addTo(map);
+    // 2) Satellite imagery 
+    let satellite = L.tileLayer(
+        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        {
+            attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, ' +
+                'Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+        }
+    );
 
-    L.marker(wbz).bindPopup("WBZ Boston - Home of the WBZ-TV/CBS Studio. There is a <a href='https://www.bostonplans.org/projects/development-projects/1170-soldiers-field-road'>plan</a> to redevelop the vicinity to include three buildings designed for office and life science research uses and one 85-unit residential building.").addTo(map);
+    // Add default base layer to the map
+    osm.addTo(map);
 
-})
+
+
+
+    /* Overlays */
+
+    let smithMarker = L.marker(smithPlayground)
+        .bindPopup("Smith Playground - <a href='https://www.boston.gov/news/smith-playground-allston-reopened'>Renovated in 2019,</a> there are amenities for many sports, including 2 basketball courts, 2 softball fields, a skateboarding track, a street hockey and roller skating/rollerblading rink, an athletic track, and plenty of open field space for soccer and other activities.");
+
+    let harvardMarker = L.marker(harvardStadium)
+        .bindPopup("Harvard Stadium - Where the Harvard football team plays. For a grueling workout, try the <a href='https://medium.com/@michaelwoodfitness/one-of-the-best-workouts-you-can-do-stadium-stair-running-or-walking-3d86c4a245c3'>'tour de stade'</a>, where participants ascend and descend the stairs in each seating section (for a total of ~1,150 steps).");
+
+    let wbzMarker = L.marker(wbz)
+        .bindPopup("WBZ Boston - Home of the WBZ-TV/CBS Studio. There is a <a href='https://www.bostonplans.org/projects/development-projects/1170-soldiers-field-road'>plan</a> to redevelop the vicinity to include three buildings designed for office and life science research uses and one 85-unit residential building.");
+
+    // Group markers into one overlay
+    let pointsOfInterest = L.layerGroup([smithMarker, harvardMarker, wbzMarker]).addTo(map);
+
+
+
+
+    /* --- Layer Control --- */
+
+    let baseMaps = {
+        "Street Map": osm,
+        "Satellite": satellite
+    };
+
+    let overlayMaps = {
+        "Points of Interest": pointsOfInterest
+    };
+
+    L.control.layers(baseMaps, overlayMaps).addTo(map);
+});
